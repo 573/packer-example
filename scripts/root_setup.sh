4b202c85eb5ce84837fff793f61e6bf7fc7f1acd
Daniel Kahlenberg <daniel.kahlenberg@mgm-tp.com>
2018-10-23 13:58:32 +0200

    WIP: Updated to Ubuntu 17.10.1

--- scripts/root_setup.sh
+++ scripts/root_setup.sh
@@ -2,6 +2,19 @@
 
 set -e
 
+# i. e. saucy salamander
+###sudo cp /etc/apt/sources.list /etc/apt/sources.list.17101
+###sudo cat > /etc/apt/sources.list << EOF
+## EOL upgrade sources.list
+# Required
+###deb http://old-releases.ubuntu.com/ubuntu/ artful main restricted universe multiverse
+###deb http://old-releases.ubuntu.com/ubuntu/ artful-updates main restricted universe multiverse
+###deb http://old-releases.ubuntu.com/ubuntu/ artful-security main restricted universe multiverse
+
+# Optional
+#deb http://old-releases.ubuntu.com/ubuntu/ artful-backports main restricted universe multiverse
+###EOF
+
 # Updating and Upgrading dependencies
 sudo apt-get update -y -qq > /dev/null
 sudo apt-get upgrade -y -qq > /dev/null
@@ -10,7 +23,7 @@ sudo apt-get upgrade -y -qq > /dev/null
 sudo apt-get -y -q install linux-headers-$(uname -r) build-essential dkms nfs-common
 
 # Install necessary dependencies
-sudo apt-get -y -q install curl wget git tmux firefox xvfb vim
+sudo apt-get -y -q install curl wget git tmux vim
 
 # Setup sudo to allow no-password sudo for "admin"
 groupadd -r admin
@@ -19,19 +32,3 @@ cp /etc/sudoers /etc/sudoers.orig
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=admin' /etc/sudoers
 sed -i -e 's/%admin ALL=(ALL) ALL/%admin ALL=NOPASSWD:ALL/g' /etc/sudoers
 
-#Install Redis
-sudo apt-get -y -q install libjemalloc1
-wget -q http://d7jrzzvab3wte.cloudfront.net/checkbot/deb/redis-server_2.6.13-1_amd64.deb
-sha1sum redis-server_2.6.13-1_amd64.deb | grep 'ab50cf037fd63e160946f8946b6d318cdf11800d'
-dpkg -i redis-server_2.6.13-1_amd64.deb
-rm redis-server_2.6.13-1_amd64.deb
-
-# Install required libraries for RVM and Ruby
-sudo apt-get -y -q install gawk libreadline6-dev zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 autoconf libgdbm-dev libncurses5-dev automake libtool bison pkg-config libffi-dev libxml2-dev libxslt-dev libxml2
-
-
-# Install Postgresql
-sudo apt-get -y -q install postgresql libpq-dev postgresql-contrib
-
-# Set Password to test for user postgres
-sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'test';"
